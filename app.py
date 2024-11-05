2213036e7b875e574828331756e1f371ef1cd847
Nithin G <79520840+2nithing@users.noreply.github.com>
2024-11-05 05:51:15 +0000

    2

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import streamlit as st
 from PIL import Image
 
 st.header("Vitamin Deficiency Detection")
-
-st.sidebar.subheader('Upload your Image')
-img = st.sidebar.file_uploader('Upload Skin, Nail, Toungue, or Eye Image',type=['png','jpg','jpeg'])
-st.sidebar.write('Powered by AI and Streamlit')
+sb = st.sidebar
+sb.subheader('Upload your Image')
+img = sb.file_uploader('Upload Skin, Nail, Toungue, or Eye Image',type=['png','jpg','jpeg'])
+sb.write('Powered by AI and Streamlit')
 
 if img:
     st.image(img,'Uploaded Image')
-
-    if st.button("Find Deficiency"):
+    btn = st.button("Find Deficiency")
+    if btn:
         gray_image = Image.open(img).convert("L")
         st.image(gray_image,'Processed Image')
 
